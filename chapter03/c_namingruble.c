8fdd7dd68fe5173fa5588d6c9288a59e87fbe49d
mhj12378 <112968045+mhj12378@users.noreply.github.com>
2022-10-05 10:35:03 +0900

    Add files via upload

--- chapter03/c_namingruble.c
+++ chapter03/c_namingruble.c
@@ -11,5 +11,19 @@
  *  4. 예약어 사용불가
  *    -예약어란 프로그래밍 언어에서 이미 사용하고 있는 이름
  *    -예: const, unsigned, char, int, long 등등
+ * 
+ * 
+ * 10월 5일
+ * 
+ * 표기법 
+ *   - 합성어 예: 학생 이름 변수 
+ *   studentlastname  (가독성 x) 
+ * 
+ *  1.파스칼 표기법
+ *    StudentLastName
+ *  2.카멜 표기법
+ *    studentLastName
+ *  3.스네이크 표기법(최신)           파이썬 13 자바 12
+ *    student_last_name
  *  
 */
